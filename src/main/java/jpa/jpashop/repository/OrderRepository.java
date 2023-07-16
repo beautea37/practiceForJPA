0ba0ef87d9d4dfddd916dee7e73fe27be0cdb00a
beautea37 <beautea37@gmail.com>
2023-07-17 04:29:56 +0900

    Practice jpa

--- src/main/java/jpa/jpashop/repository/OrderRepository.java
+++ src/main/java/jpa/jpashop/repository/OrderRepository.java
@@ -1,14 +1,19 @@
 package jpa.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jpa.jpashop.domain.Order;
+import jpa.jpashop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
 
+    @PersistenceContext
     private  final EntityManager em;
 
     public void save(Order order) {
@@ -19,4 +24,8 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
+    public List<Order> findAll(OrderSearch orderSearch) {
+        String jpql = "select o from o from Order o join o.member m";
+
+    }
 }
