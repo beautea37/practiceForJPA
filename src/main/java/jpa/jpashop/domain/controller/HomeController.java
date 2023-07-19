045ab0d562e24e5fce9afe0f035edbfc56f06368
beautea37 <beautea37@naver.com>
2023-07-19 17:47:42 +0900

    Controller 수정

---
+++ src/main/java/jpa/jpashop/domain/controller/HomeController.java
@@ -0,0 +1,17 @@
+package jpa.jpashop.domain.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j      //log문 쓸 수 있게끔 해줌
+public class HomeController {
+
+    
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+}
